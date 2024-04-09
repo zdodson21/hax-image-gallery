fd35b91b515e35046fd0bd9cf4541d1071fa3e9d
zdodson21 <zdodson21@gmail.com>
2024-04-09 14:39:23 -0400

    boiler plate for media-image

--- src/media-image.js
+++ src/media-image.js
@@ -1,2 +1,58 @@
 import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
 import { css, html } from 'lit';
+
+/**
+ * For use under <hax-image-gallery>
+ * https://github.com/elmsln/issues/issues/1946
+ * @author {Zach Dodson}
+ */
+
+export class MediaImage extends DDD {
+    
+    static get tag() {
+        return 'media-image';
+    }
+
+    constructor() {
+        super();
+        this.src = '';
+        this.alt = '';
+        this.width = '';
+        this.height = '';
+        this.primaryColor = '';
+        this.secondaryColor = '';
+    }
+
+    static get styles() {
+        return [ 
+            super.styles,
+            css`
+                /*               
+                CSS is based around variables / standards of 'Design, Develop, Destroy' (DDD). Styleguide for DDD can be found at the link below:
+
+                https://oer.hax.psu.edu/bto108/sites/haxcellence/documentation/ddd 
+                */
+
+
+            `
+        ]
+    }
+
+    render() {
+        return html`
+            <img src="${this.src}" alt="${this.alt}" class='media-img'>
+        `
+    }
+
+    static get properties() {
+        return {
+            ...super.properties,
+            src: { String },
+            alt: { String },
+            width: { Number },
+            height: { Number },
+            primaryColor: { String },
+            secondaryColor: { String },
+        }
+    }
+}
